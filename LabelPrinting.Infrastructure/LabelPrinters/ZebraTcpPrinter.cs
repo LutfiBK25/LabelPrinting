d631ee04abdb07656308ab78726b2cfddcfd9e50
Lutfi Alfawadleh <lutfibk25@gmail.com>
2025-12-26 01:49:00 -0500

    Zebra Label Printing is coming back white empty,,,, and multiple Elements in the label in pdf is causing it not print

--- LabelPrinting.Infrastructure/LabelPrinters/ZebraTcpPrinter.cs
+++ LabelPrinting.Infrastructure/LabelPrinters/ZebraTcpPrinter.cs
@@ -1,15 +1,18 @@
-﻿using System.Net.Sockets;
+﻿using System;
+using System.Net.Sockets;
 using System.Text;
 using LabelPrinting.Domain.Entities.Label;
+using LabelPrinting.Domain.Entities.Label.Elements;
 using LabelPrinting.Domain.Interfaces;
 
 namespace LabelPrinting.Infrastructure.Printers
 {
-    // Zebra Label Configuration
     public class ZebraTcpPrinter : ILabelPrinter
     {
         private readonly string _ip;
         private readonly int _port;
+        private const int PrinterDpi = 203;       // Zebra printer dots per inch
+        private const double DesignerScale = 100; // Pixels per inch in your designer
 
         public ZebraTcpPrinter(string ip, int port)
         {
@@ -21,28 +24,55 @@ namespace LabelPrinting.Infrastructure.Printers
         {
             try
             {
-                using var client = new TcpClient(_ip, _port); // each print open a new tcp connection
-                using var stream = client.GetStream(); // tcp data pipe , write raw bytes to it
+                using var client = new TcpClient(_ip, _port);
+                using var stream = client.GetStream();
 
-                string zpl =
-                    $@"^XA
-                    ^PW812
-                    ^LL1218
-                    ^FO102,102^ADN,50,30^FD{label.Name}^FS
-                    ^FO102,200^BY3
-                    ^BCN,200,Y,N,N
-                    ^FD{label.Name}^FS
-                    ^XZ";
-
+                string zpl = BuildZpl(label);
                 var bytes = Encoding.ASCII.GetBytes(zpl);
-                stream.WriteAsync(bytes, 0, bytes.Length);
+
+                stream.Write(bytes, 0, bytes.Length);
             }
             catch (SocketException ex)
             {
-                /// ToDo program shouldn't be crashing
-                // when printer is offline (if there is no printer it will crash)
                 throw new Exception($"Cannot connect to Zebra printer at {_ip}:{_port}", ex);
             }
+        }
+
+        private string BuildZpl(Label label)
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine("^XA"); // start label
+
+            // Set label width and length in dots
+            sb.AppendLine($"^PW{ConvertToDots(label.WidthInches)}");
+            sb.AppendLine($"^LL{ConvertToDots(label.HeightInches)}");
+
+            // Loop through all label elements
+            foreach (var serializableElement in label.Elements)
+            {
+                var element = serializableElement.ToDomain();
+
+                if (element is LabelTextElement text)
+                {
+                    int x = ConvertToDots(text.X);
+                    int y = ConvertToDots(text.Y);
+                    int fontHeight = ConvertToDots(text.FontSize); // scale font size
+
+                    // ^ADN,fontHeight,width -> width=0 lets printer scale proportionally
+                    sb.AppendLine($"^FO{x},{y}^ADN,{fontHeight},0^FD{text.Text}^FS");
+                }
+
+                // TODO: add support for barcodes or images here
+            }
+
+            sb.AppendLine("^XZ"); // end label
+            return sb.ToString();
+        }
+
+        private int ConvertToDots(double designerPixels)
+        {
+            // Convert from designer pixels to printer dots
+            return (int)(designerPixels / DesignerScale * PrinterDpi);
         }
     }
 }
